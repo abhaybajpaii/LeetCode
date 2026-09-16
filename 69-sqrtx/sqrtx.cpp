8c47077626323514cddcba6c1eb0670fdc63eba9
Abhay Bajpai <abhay.bajpai2024@lpu.in>
2026-09-16 23:08:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (14.17%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x < 2) return x;
+
+        long long l = 1, r = x / 2;
+
+        while (l <= r) {
+            long long m = l + (r - l) / 2;
+
+            if (m * m == x)
+                return m;
+
+            if (m * m < x)
+                l = m + 1;
+            else
+                r = m - 1;
+        }
+
+        return r;
+    }
+};
